60c6663e14cbecc8050ae3a983b4eaacfed5f72e
Thomas Blakely <YlekalbSamoht@gmail.com>
2025-09-24 13:28:19 -0700

    Dog Script

---
+++ data-processing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# 5 Dog Breeds
+dogs = ["Beagle",  "Labrador Retriever", "German Shepherd", "Poodle", "Golden Retriever"]
+
+# Output Dog Breeds
+for breed in dogs:
+    print(breed)
