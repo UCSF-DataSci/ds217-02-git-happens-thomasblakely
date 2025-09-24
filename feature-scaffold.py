ef5d0b00b0edd96498430fdc11dd8be933feb2b3
Thomas Blakely <YlekalbSamoht@gmail.com>
2025-09-24 13:30:18 -0700

    Cat Script

---
+++ feature-scaffold.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# 5 Cat Breeds
+cats = ["Siamese cat", "British Shorthair", "Maine Coon", "Persian cat", "Ragdoll"]
+
+# Output Cat Breeds
+for breed in cats:
+    print(breed)
+
