417bb2873cd255a97435cffe4ba1b767f383c9ef
Thomas Blakely <YlekalbSamoht@gmail.com>
2025-10-01 03:33:57 -0700

    Add files made from .sh to branch

---
+++ src/data_analysis.py
@@ -0,0 +1,18 @@
+def load_students(filename):
+    """Read CSV and return list of student data"""
+    # TODO - Complete Function
+def calculate_average_grade(students):
+    """Calculate and return average"""
+    # TODO - Complete Function
+def count_math_students(students):
+    """Count students in Math"""
+    # TODO - Complete Function
+def generate_report():
+    """Create formatted report string"""
+    # TODO - Complete Function
+def save_report(report, filename): 
+    """Write report to file"""
+    # TODO - Complete Function
+def main():
+    """Orchestrate the analysis"""
+    # TODO - Complete Function
---
+++ src/data_analysis_functions.py
@@ -0,0 +1,18 @@
+def load_data(filename):
+    """Generic loader that checks file extension"""
+    # TODO - Complete Function
+def load_csv(filename):
+    """Load CSV data (same technique as basic script)"""
+    # TODO - Complete Function
+def analyze_data(students):
+    """Return dictionary with multiple statistics"""
+    # TODO - Complete Function
+def analyze_grade_distribution(grades):
+    """Count grades by letter grade ranges"""
+    # TODO - Complete Function
+def save_results(results, filename):
+    """Save detailed report"""
+    # TODO - Complete Function
+def main():
+    """Orchestrate the analysis using all functions"""
+    # TODO - Complete Function
